913e0f8df348a761c67c9723253e0c33f4b370cc
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2025-07-01 16:35:34 -0400

    Changed About Me, Added pictures, Changed Contact Page

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,18 @@ export default function About() {
     <section className="about">
       <h2>About Me</h2>
       <p>
-        I’m a Computer Science student at Florida State University graduating in May 2025, with a strong background
+        I’m a Computer Science B.S. Graduate from Florida State University with a strong background
         in full-stack development, software design, and Agile collaboration. I’ve led C++ and Blazor projects,
-        mentored peers in best practices, and built apps ranging from LMS platforms to AI-powered quizzes.
+        mentored peers in best practices, and built python apps ranging from song mood boards to AI-powered quizzes.
       </p>
+      <div className="about-images">
+        <div className="grad-image">
+          <img src="/images/Graduation.jpg" alt="Graduation" className="grad-image" />
+        </div>
+        <div className="head-image">
+          <img src="/images/portfolio.JPG" alt="Headshot" className="head-image" />
+        </div>
+      </div>
     </section>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,17 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 export default function Contact() {
   return (
+    <>
+    <Header />
     <section className="contact-page">
       <h2>Contact Me</h2>
       <p>Email: corsomontuori@gmail.com</p>
       <p>LinkedIn: <a href="https://linkedin.com/in/corsomontuori" target="_blank" rel="noopener noreferrer">linkedin.com/in/corsomontuori</a></p>
       <p>GitHub: <a href="https://github.com/Corsionio" target="_blank" rel="noopener noreferrer">github.com/Corsionio</a></p>
     </section>
+    <Footer />
+   </>
   );
 }
