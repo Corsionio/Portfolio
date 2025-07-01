a9c3430d88e14232ec15354a08ffa6b3dfa455ae
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2025-07-01 16:47:03 -0400

    Changed About Me, Changed bordering on letters and boxes

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,9 @@ export default function About() {
         I’m a Computer Science B.S. Graduate from Florida State University with a strong background
         in full-stack development, software design, and Agile collaboration. I’ve led C++ and Blazor projects,
         mentored peers in best practices, and built python apps ranging from song mood boards to AI-powered quizzes.
+        My background in customer service enhances my ability to communicate clearly and effectively with team members, clients, and stakeholders in any setting.
       </p>
       <div className="about-images">
-        <div className="grad-image">
-          <img src="/images/Graduation.jpg" alt="Graduation" className="grad-image" />
-        </div>
         <div className="head-image">
           <img src="/images/portfolio.JPG" alt="Headshot" className="head-image" />
         </div>
