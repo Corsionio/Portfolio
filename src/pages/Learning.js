9d730ba93c618c0fc39bb31f125f2fea2d794894
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2025-07-02 14:05:17 -0400

    Added fade in for contact, box fade in for currently learning and contact

--- src/pages/Learning.js
+++ src/pages/Learning.js
@@ -2,7 +2,16 @@ import { motion } from 'framer-motion';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const containerVariants = {
+const outerBoxVariants = {
+  hidden: { opacity: 0, y: 30 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.6 },
+  },
+};
+
+const contentVariants = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
@@ -22,22 +31,28 @@ export default function Learning() {
     <>
       <Header />
       <motion.section
-        className="Learning-page"
-        variants={containerVariants}
+        className="learning-page"
+        variants={outerBoxVariants}
         initial="hidden"
         animate="show"
       >
-        <h2>What I'm Studying Currently</h2>
+        <motion.div
+          variants={contentVariants}
+          initial="hidden"
+          animate="show"
+        >
+          <motion.h2 variants={itemVariants}>What I'm Studying Currently</motion.h2>
 
-        <motion.p variants={itemVariants}>
-          <b>Languages:</b> Rust, Diving deeper into: Python
-        </motion.p>
-        <motion.p variants={itemVariants}>
-          <b>Frameworks:</b> Angular
-        </motion.p>
-        <motion.p variants={itemVariants}>
-          <b>Libraries:</b> Tensorflow, Diving deeper into: pyTorch
-        </motion.p>
+          <motion.p variants={itemVariants}>
+            <b>Languages:</b> Rust, Diving deeper into: Python
+          </motion.p>
+          <motion.p variants={itemVariants}>
+            <b>Frameworks:</b> Angular
+          </motion.p>
+          <motion.p variants={itemVariants}>
+            <b>Libraries:</b> Tensorflow, Diving deeper into: pyTorch
+          </motion.p>
+        </motion.div>
       </motion.section>
       <Footer />
     </>
