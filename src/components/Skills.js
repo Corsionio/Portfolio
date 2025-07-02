0914c1ef97fbcac076994968d9b446a8ba5107aa
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2025-07-02 01:04:09 -0400

    Changed skills to reflect current

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,10 +2,10 @@ export default function Skills() {
   return (
     <section className="skills">
       <h2>Technical Skills</h2>
-      <p><strong>Languages:</strong> Python, C/C++, SQL, JavaScript/TypeScript, C#, Java, HTML/CSS, Ruby</p>
+      <p><strong>Languages:</strong> Python, C/C++, SQL (mySQL, PostgreSQL), JavaScript/TypeScript, C#, Java, HTML/CSS, Ruby, <b><u>React</u></b></p>
       <p><strong>Frameworks:</strong> Django, Flask, FastAPI, .NET, Blazor, Spring</p>
-      <p><strong>Tools:</strong> Git, Github, Microsoft Azure, Docker, AWS, Google Cloud Platform, Unix, Linux, VS Code, 
-      Visual Studio, pyCharm, Ubuntu, SSH, Linux, Machine Learning, Statistical Analysis</p>
+      <p><strong>Tools:</strong> Git, Github, Microsoft Azure, Docker, AWS, Google Cloud Platform, Unix, VS Code, 
+      Visual Studio, pyCharm, Ubuntu, SSH, Linux, Machine Learning, Statistical Analysis, CI/CD, DevTools, DevOps</p>
       <p><strong>Libraries:</strong> NumPy, pandas, pyTorch, OpenAI, Matplotlib, dotenv, json, C++ STL</p>
     </section>
   );
