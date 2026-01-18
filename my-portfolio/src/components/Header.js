1079037c9d4f0265da8206b290b19d894fba5f98
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2026-01-18 17:04:15 -0500

    Fixed compile errors

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 export default function ScrollingHeader() {
   const text = "Corso Montuori's Portfolio";
   const repeatedText = `${text} • `.repeat(20);
